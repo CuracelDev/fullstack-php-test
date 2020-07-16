07ddd572084f4465b6e85991b36de27695d0ba45
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 18:24:14 +0100

    Coupon model

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Coupon extends Model
 {
-    //
+	use SoftDeletes;
+
+    protected $table = 'coupons';
+	
+	protected $fillable = [ 'code', 'tax'];
+
+    protected $dates = ['deleted_at'];
 }
