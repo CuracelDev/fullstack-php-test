4a17fa419963fa91dd8ea4b3fc07a0bed1f34255
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 18:24:48 +0100

    Order  model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    //
+	use SoftDeletes;
+
+    protected $table = 'orders';
+	
+	protected $fillable = ['product_id', 'user_id', 'price'];
+
+    protected $dates = ['deleted_at'];
+
+    public function product() {
+    	return $this->belongsTo(Product::class, 'products', 'id');
+    }
+
+    public function user() {
+    	return $this->belongsTo(User::class, 'users', 'id');
+    }
 }
