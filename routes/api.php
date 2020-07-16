a70ea6c52f5fb67d9240bb0c9b738f9dec61427a
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 19:30:19 +0100

    Prepare all endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+	Route::get('/welcome', 'UserController@welcome');
+	Route::post('/login', 'UserController@login');
+	Route::post('/register', 'UserController@register');
+	
+	Route::get('/products', 'ProductController@products');
+	Route::post('/products/add', 'ProductController@add');
+	
+	Route::get('/coupons', 'CouponController@coupons');
+	Route::post('/coupons/add', 'CouponController@add');
+	
+	Route::get('/orders', 'OrderController@orders');
+	Route::post('/orders/add', 'OrderController@add');
+	Route::delete('/orders/delete/{orderId}', 'OrderController@delete');
 });
