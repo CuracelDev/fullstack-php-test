46a102f987b8505495736927b249229eecdfdb0a
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 18:25:37 +0100

    User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
@@ -15,8 +18,19 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    protected $table = 'users';
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 
+        'email', 
+        'password', 
+        'email_verified_at', 
+        'age', 
+        'age_status', 
+        'tax_status', 
+        'tax', 
+        'remember_token'
     ];
 
     /**
@@ -36,4 +50,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = ['deleted_at'];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
 }
