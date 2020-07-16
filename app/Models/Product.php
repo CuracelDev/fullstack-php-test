6d4c9a0724d88d18e29911fb10639a8f124e4e19
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 18:25:16 +0100

    Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+	use SoftDeletes;
+
+    protected $table = 'products';
+	
+	protected $fillable = [
+    	'name',
+    	'price',
+    	'pix',
+    	'age_limit_status',
+    	'start_age_range',
+    	'end_age_range',
+    	'coupon_status',
+    	'coupon_id',
+    	'frequency_limit_status',
+    	'frequency_limit'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
 }
