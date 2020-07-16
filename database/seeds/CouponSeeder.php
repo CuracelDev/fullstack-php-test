f231fbb4204ede6563be3a83e65f453a04215134
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 21:19:19 +0100

    Create coupon seeder

---
+++ database/seeds/CouponSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Http\CommonHelper;
+
+class CouponSeeder extends Seeder
+{
+    use CommonHelper;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for($i = 0; $i < 50; $i++) {
+
+            App\Models\Coupon::create([
+                'code' => $this->generateRandomChars(15),
+                'tax' => rand(10, 80),
+            ]);
+        }
+    }
+}
