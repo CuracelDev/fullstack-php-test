2fd4d75a2873341072512b83e0c3fda02e020e33
ayodeleoniosun <heywire4u@gmail.com>
2020-07-16 17:18:31 +0100

    Products migration table

--- database/migrations/2020_07_10_003532_create_products_table.php
+++ database/migrations/2020_07_10_003532_create_products_table.php
@@ -15,7 +15,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->longText('name');
+            $table->string('price');
+            $table->string('pix');
+            $table->enum('age_limit_status', ['yes', 'no']);
+            $table->string('start_age_range')->nullable();
+            $table->string('end_age_range')->nullable();
+            $table->enum('coupon_status', ['yes', 'no']);
+            $table->string('coupon_id')->nullable();
+            $table->enum('frequency_limit_status', ['yes', 'no']);
+            $table->string('frequency_limit')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
