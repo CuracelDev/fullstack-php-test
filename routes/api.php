3c80fe01510340f109c1f781e773753c18dfbd4b
fakorede <abiolafakorede@gmail.com>
2020-07-21 22:24:10 +0100

    Endpoints for showing all and single products

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('products', function (Request $request) {
+    return Product::all();
+});
+
+Route::get('products/{id}', function (Request $request, $id) {
+    return Product::find($id);
 });
