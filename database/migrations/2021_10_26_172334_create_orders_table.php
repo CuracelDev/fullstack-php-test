2cce66e57b04a90ee3c778135b66de08ffc46a31
John Flash <Johngood4real@yahoo.co.in>
2021-10-26 18:31:48 +0100

    Add items column to orders table

--- database/migrations/2021_10_26_172334_create_orders_table.php
+++ database/migrations/2021_10_26_172334_create_orders_table.php
@@ -15,6 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->json('items');
             $table->timestamps();
         });
     }
