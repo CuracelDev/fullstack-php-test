a13008b5d4092427cbb35f4a0d3fe5bc62becd6b
John Flash <Johngood4real@yahoo.co.in>
2021-10-26 17:36:25 +0100

    Rename default vue component

--- resources/js/components/SubmitOrder.vue
+++ resources/js/components/SubmitOrder.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Submit An Order</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Order details here
                     </div>
                 </div>
             </div>
