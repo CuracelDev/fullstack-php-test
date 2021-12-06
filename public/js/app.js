f27834c68eef61b0e20a94b2b7a4d241cd04a4ff
anthonyogundipeshr <anthonyogundipe@seamlesshr.com>
2021-12-06 09:29:33 +0100

    Completed the exercise

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/drflash/www/curacel/fullstack-php-test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/drflash/www/curacel/fullstack-php-test/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/tony/dev/www/fullstack-php-test/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/tony/dev/www/fullstack-php-test/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
