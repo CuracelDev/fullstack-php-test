d636fcbfd567908b8b63c3b81706e3b05b86d396
Joshua Ayeni <larryjmpk@gmail.com>
2022-05-20 10:36:19 +0100

    created controllers

--- app/Http/Controllers/HmoController.php
+++ app/Http/Controllers/HmoController.php
@@ -5,24 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class OrderController extends Controller
+class HmoController extends Controller
 {
     
 
     public function create(Request $request)
     {
         $v = Validator::make($request->all(),[
-            'provider_id'=>'required|exists:users,id',
-            'hmo_id'=> 'required|exists:hmos,id',
-            'items'=>'required'
+            'code'=>'required|unique:hmos,code',
+            'name'=> 'required|string',
         ]);
 
         if($v->fails()){
             return response()->json(['message'=>$v->messages()],422);
         }
-
-        //creare order
-
-
     }
 }
---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProviderController extends Controller
+{
+    
+    public function create(Request $request)
+    {
+
+    }
+}
