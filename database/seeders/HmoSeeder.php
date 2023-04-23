031d5525f9ac528f09f450c942bea58c25cddf9a
prismathic <pjjsalam1999@gmail.com>
2023-04-23 22:22:43 +0100

    update `HmoSeeder` with batch preference field

--- database/seeders/HmoSeeder.php
+++ database/seeders/HmoSeeder.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\DB;
 class HmoSeeder extends Seeder
 {
     private $hmos = [
-        ['name'=>'HMO A', 'code'=> 'HMO-A'],
-        ['name'=>'HMO B', 'code'=> 'HMO-B'],
-        ['name'=>'HMO C', 'code'=> 'HMO-C'],
-        ['name'=>'HMO D', 'code'=> 'HMO-D'],
+        ['name' => 'HMO A', 'code' => 'HMO-A', 'batch_by_encounter_date' => false],
+        ['name' => 'HMO B', 'code' => 'HMO-B', 'batch_by_encounter_date' => true],
+        ['name' => 'HMO C', 'code' => 'HMO-C', 'batch_by_encounter_date' => true],
+        ['name' => 'HMO D', 'code' => 'HMO-D', 'batch_by_encounter_date' => true],
     ];
 
     /**
