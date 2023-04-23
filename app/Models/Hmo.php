475852c153d257f0e7095ab197c3293c3a5e5115
prismathic <pjjsalam1999@gmail.com>
2023-04-23 22:23:44 +0100

    add necessary casts and relationships to models

--- app/Models/Hmo.php
+++ app/Models/Hmo.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Hmo extends Model
 {
-    //
+    protected $casts = [
+        'batch_by_encounter_date' => 'boolean',
+    ];
+
+    protected $guarded = ['id'];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
-    //
+    protected $casts = [
+        'items' => 'array',
+    ];
+
+    protected $guarded = ['id'];
+
+    public function hmo(): BelongsTo
+    {
+        return $this->belongsTo(Hmo::class);
+    }
 }
