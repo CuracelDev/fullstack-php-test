af44b7170ec9a897982277f0713fc566b3b3fbbe
prismathic <pjjsalam1999@gmail.com>
2023-04-24 20:59:53 +0100

    add custom error message for duplicate item

--- app/Http/Requests/Provider/SubmitOrderRequest.php
+++ app/Http/Requests/Provider/SubmitOrderRequest.php
@@ -24,14 +24,40 @@ class SubmitOrderRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string'],
+            'name' => ['required', 'string', 'min:2', 'max:20'],
             'hmo' => ['required', 'string', 'exists:hmos,code'],
             'encounter_date' => ['required', 'date', 'before_or_equal:today'],
             'items' => ['required', 'array'],
             'items.*' => ['required', 'array'],
-            'items.*.item' => ['required', 'string', 'distinct:ignore_case'],
+            'items.*.item' => ['required', 'string', 'min:2', 'max:50', 'distinct:ignore_case'],
             'items.*.price' => ['required', 'numeric', 'gt:0'],
             'items.*.quantity' => ['required', 'integer', 'gt:0'],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'items.*.item' => 'item',
+            'items.*.price' => 'price',
+            'items.*.quantity' => 'quantity',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'items.*.item.distinct' => 'One of the items has been entered twice. You can fix this by removing the duplicate item',
         ];
     }
 }
