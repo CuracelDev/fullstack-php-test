dad4ec56c5046eb0b72545932a78ba30cebb4e76
prismathic <pjjsalam1999@gmail.com>
2023-04-24 09:45:56 +0100

    add route and controller/action logic

---
+++ app/Actions/Provider/SubmitOrder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Actions\Provider;
+
+use App\Http\Requests\Provider\SubmitOrderRequest;
+use App\Models\Hmo;
+use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class SubmitOrder
+{
+    use AsAction;
+
+    public function handle(array $submitOrderData): Order
+    {
+        $hmo = Hmo::findByCode($submitOrderData['hmo']);
+
+        ['name' => $provider, 'encounter_date' => $encounterDate] = $submitOrderData;
+
+        return $hmo->orders()->create([
+            'provider' => $provider,
+            'items' => data_get($submitOrderData, 'items'),
+            'encounter_date' => $encounterDate,
+            'status' => Order::STATUS_PENDING,
+        ]);
+    }
+
+    public function asController(SubmitOrderRequest $request): JsonResponse
+    {
+        try {
+            $this->handle($request->validated());
+        } catch (\Throwable $th) {
+            report($th); //log exception for monitoring.
+
+            return $this->jsonResponse(
+                'An error occurred while attempting to submit your order.',
+                JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        //send the mail here
+
+        return $this->jsonResponse('Order submitted successfully.');
+    }
+
+    private function jsonResponse(string $message, int $statusCode = JsonResponse::HTTP_OK): JsonResponse
+    {
+        $status = $statusCode >= 200 && $statusCode < 400;
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Actions\Provider\SubmitOrder;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('orders')->name('orders.')->group(function () {
+    Route::post('/', SubmitOrder::class)->name('submit');
 });
