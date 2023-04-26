53ad28d9669b2de9ddf553b347c088e055d7c16e
Celestine Stephen <decele2011@gmail.com>
2023-04-26 12:38:14 +0100

    fix example test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,17 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
 
-    $response->assertStatus(200);
-});
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
