15fc701b7fe24eb352a6795dd46f1bbe6aa6d3fe
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2023-12-14 09:48:49 +0100

    test for invalid data in submission

--- tests/Feature/SubmitOrderTest.php
+++ tests/Feature/SubmitOrderTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Events\OrderSubmitted;
 use App\Models\Hmo;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
@@ -37,6 +39,32 @@ class SubmitOrderTest extends TestCase
         ]);
 
         $response->assertStatus(200)->assertExactJson(['success' => true, 'message' => 'Order processed']);
+    }
+
+    /** @test */
+    public function test_that_the_submission_triggers_an_order_submitted_event(): void
+    {
+        Event::fake();
+    
+        $this->postJson('/api/submit-order', [
+            'hmo_code' => 'HMO-D',
+            'provider_name' => 'Provider B',
+            'encounter_date' => '15-10-2023',
+            'items'=> [
+                [
+                    'name' => 'Apple',
+                    'unit_price' => 10,
+                    'quantity' => 3
+                ],
+                [
+                    'name' => 'Orange',
+                    'unit_price' => 4,
+                    'quantity' => 15
+                ]
+            ]
+        ]);
+    
+        Event::assertDispatched(OrderSubmitted::class);
     }
 
     /** @test */
@@ -88,8 +116,38 @@ class SubmitOrderTest extends TestCase
             'order_amount' => 90,
             'encounter_date' => Carbon::parse($data['encounter_date'])->toDateTimeString(),
             'batch_id' => null
+        ]);  
+    }
+
+        /** @test */
+    public function test_that_invalid_data_is_not_submitted(): void
+    {
+        Event::fake();
+
+        $response = $this->postJson('/api/submit-order', [
+            'hmo_code' => 'HMO-D',
+            'provider_name' => '',
+            'encounter_date' => 111,
+            'items'=> [
+                [
+                    'name' => 'Apple',
+                    'unit_price' => [],
+                    'quantity' => 3
+                ],
+                [
+                    'name' => 'Orange',
+                    'unit_price' => 4,
+                    'quantity' => 'jfjfhf'
+                ]
+            ]
         ]);
 
-        
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonValidationErrors(['provider_name', 'encounter_date', 'items.0.unit_price', 'items.1.quantity']);
+        $this->assertDatabaseMissing('orders', [
+            'provider_name' => '',
+            'encounter_date' => 111,
+        ]);
+        Event::assertNotDispatched(OrderSubmitted::class);
     }
 }
