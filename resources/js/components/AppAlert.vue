d245d6386d6d2e04da5ba42348af5ac4de34e966
Adedayo Matthews <adedayo.kayode@konga.com>
2024-01-04 14:32:10 +0100

    Add basic button, input and alert components

---
+++ resources/js/components/AppAlert.vue
@@ -0,0 +1,14 @@
+<template>
+    <div  :class="`alert alert-${type}`" role="alert">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppAlert",
+    props: {
+        type: String
+    }
+}
+</script>
